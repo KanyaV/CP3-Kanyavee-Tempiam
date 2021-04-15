4a1f2fee7f3053928fc0121acac9da20cd41c2fb
KanyaV <kanyavid@gmail.com>
2021-04-15 23:27:53 +0700

    Exercise4
    Send Homework Exercise4

---
+++ Exercise4_Kanyavee_T.py
@@ -0,0 +1,11 @@
+Name = "Kanyavee.T"
+Fou_Eng = 89
+Gel_Bus = 92
+Intro_Com_Sys = 95
+Com_Pro = 88
+
+print(Name, "Score")
+print("Foundation English : ", Fou_Eng)
+print("General Business : ", Gel_Bus)
+print("Introduction to Computer Systems : ", Intro_Com_Sys)
+print("Computer Programming : ", Com_Pro)
