1a8bfe82a621b2d066c5ab0dce6193f5b0f3aa18
KanyaV <kanyavid@gmail.com>
2021-04-17 19:11:01 +0700

    Send Exercise 5_1
    Send Exercise 5_1

---
+++ Exercise5_1_Kanyavee_T.py
@@ -0,0 +1,7 @@
+print("CALCULATE Function")
+num1 = int(input("Insert Number 1 : "))
+num2 = int(input("Insert Number 2 : "))
+print(num1, "+", num2, "= ", num1+num2)
+print(num1, "-", num2, "= ", num1-num2)
+print(num1, "*", num2, "= ", num1*num2)
+print(num1, "/", num2, "= ", num1/num2)
